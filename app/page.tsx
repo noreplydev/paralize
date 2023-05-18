40bf9c3c70302ebf764c4661f77e4db5b40b4ac8
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2023-05-18 23:12:43 +0200

    [FT] Added animations to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 export default function Home() {
   return (
     <main className="h-screen w-screen bg-neutral-950 flex flex-col items-center justify-center
-      bg-grid bg-cover">
-      <h1 className="font-medium text-[#FFB23E] text-5xl mb-6 z-10">Paralize</h1> 
-      <h1 className="font-light text-white text-2xl mb-10 z-10">Live demo of Next.js 13 parallel routes</h1> 
+      bg-grid bg-cover bg-no-repeat bg-center will-change-contents">
+      <h1 className="font-medium text-[#FFB23E] text-5xl mb-6 z-10 animate-scale-down
+        opacity-0"
+      >Paralize</h1> 
+      <h1 className="font-light text-white text-2xl mb-10 z-10 text-center
+        px-6 animate-scale-down-md opacity-0"
+      >Live demo of Next.js 13 parallel routes</h1> 
       <div className="absolute top-1/3 h-48 w-64 bg-[#ffb23e21] blur-3xl z-0"></div>
       <p className="text-[#d9d9d9] px-5 py-2 rounded-md border-solid border 
-        border-[#ffb23e] bg-[#ffb23e21] cursor-pointer z-10"
+        border-[#ffb23e] bg-[#ffb23e21] cursor-pointer z-10 animate-scale-down-lg opacity-0"
       >Go to 
-        <span className="text-[#ffb23e] font-light px-2"
+        <span className="text-[#ffb23e] font-light px-2 "
         >/start
         </span> 
       to see parallel routes</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
     extend: {
       backgroundImage: {
         'grid': 'url("/assets/svg/grid.svg")'
+      },
+      keyframes: {
+        'scale-down': {
+          '0%': { transform: 'scale(1.1)', opacity: 0 },
+          '100%': { transform: 'scale(1)', opacity: 1 },
+        }
+      }, 
+      animation: {
+        'scale-down': 'scale-down 0.12s 1s ease-out forwards', 
+        'scale-down-md': 'scale-down 0.12s 1.20s ease-out forwards', 
+        'scale-down-lg': 'scale-down 0.12s 1.40s ease-out forwards',
       }
     }
   },
