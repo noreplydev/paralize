cf63f98c011b4c482cc144c294a82fce35ff435c
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2023-05-31 00:18:39 +0200

    [FT] Now the input changes the url search param

--- components/dashboard/Projects.tsx
+++ components/dashboard/Projects.tsx
@@ -1,9 +1,14 @@
 import SearchBar from './Searchbar'
 
-export default function Projects () {
+export default function Projects ({ searchParams }: any) {
   return (
     <div>
       <SearchBar />
+      <ProjectsGrid searchParam={searchParams?.search || ''} />
     </div>
   )
+}
+
+function ProjectsGrid ({ searchParam }: { searchParam: string }) {
+  return <div>{searchParam}</div>
 }
--- components/dashboard/Searchbar.tsx
+++ components/dashboard/Searchbar.tsx
@@ -1,9 +1,16 @@
 'use client'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useDebouncedCallback } from 'use-debounce'
 
 export default function SearchBar () {
-  const debounced = useDebouncedCallback(values => {
-    console.log(values)
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+
+  const debounced = useDebouncedCallback(value => {
+    const params = new URLSearchParams(searchParams.toString())
+    params.set('search', value)
+    router.replace(`${pathname}?${params}`)
   }, 1000)
 
   return (
