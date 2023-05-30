034cced65abd7009183299185a782549707eff92
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2023-05-31 00:29:35 +0200

    [FT] Solved Session type on UserPin component prop

--- lib/session.ts
+++ lib/session.ts
@@ -1,8 +1,9 @@
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from './authOptions'
+import { Session } from '@/types/Session'
 
 export async function getCurrentSession() {
   const session = await getServerSession(authOptions)
 
-  return session 
+  return session as Session
 }
