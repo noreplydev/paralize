183daa354ec6d8ebb89a0ad627930d9ff84c732e
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2023-06-03 18:59:02 +0200

    [FIX] Text overlfow on user modal

--- components/dashboard/UserPin.tsx
+++ components/dashboard/UserPin.tsx
@@ -13,40 +13,30 @@ const UserModal = () => {
     <div
       id='modal'
       className='absolute top-16 right-0 w-fit h-fit bg-primary 
-        rounded-lg shadow-lg px-3 py-3.5 md:mx-24 md:my-12 cursor-pointer z-10
+        rounded-lg px-3 py-3.5 z-10 flex flex-col justify-start items-center
         border-solid border-[1px] border-secondary'
     >
       {ModalOptions.map((option, key) => {
+        const color =
+          option.name !== 'Logout' ? 'text-[#c3c3c3]' : 'text-[#DE3939]'
+
         return (
           <div
             key={key}
-            className='relative flex flex-row h-fit w-full px-4 py-2 
+            className='relative flex flex-row justify-start items-center h-fit w-full px-4 py-2 
               hover:bg-secondary cursor-pointer rounded-md transition-all duration-90 
               ease-in-out gap-3'
           >
-            <Image
-              src={option.icon}
-              height={20}
-              width={20}
-              alt='Logout image'
-            />
-            {option.name !== 'Logout' ? (
-              <p
-                key={key}
-                className={`text-[#c3c3c3] font-light`}
-                onClick={() => signOut()}
-              >
-                {option.name}
-              </p>
-            ) : (
-              <p
-                key={key}
-                className={`text-[#DE3939] font-light`}
-                onClick={() => signOut()}
-              >
-                {option.name}
-              </p>
-            )}
+            <div className='relative h-fit w-5 aspect-square'>
+              <Image src={option.icon} fill={true} alt='Logout image' />
+            </div>
+            <p
+              key={key}
+              className={`${color} font-light whitespace-nowrap text-base select-none`}
+              onClick={() => signOut()}
+            >
+              {option.name}
+            </p>
           </div>
         )
       })}
@@ -76,7 +66,10 @@ export default function UserPin ({ session }: { session: Session }) {
   }, [showModal])
 
   return (
-    <>
+    <div
+      className='relative h-fit w-fit flex flex-col justify-start items-start
+      '
+    >
       <div
         onClick={() => setShowModal(true)}
         className='relative flex flex-row justify-center items-center h-fit w-fit 
@@ -97,6 +90,6 @@ export default function UserPin ({ session }: { session: Session }) {
         <Image src={arrow} height={12} width={7} alt='user image' />
       </div>
       {showModal && <UserModal />}
-    </>
+    </div>
   )
 }
